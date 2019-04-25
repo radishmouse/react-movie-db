b9c8545a96106a7a9ae75558ac7ebacc528847f0
chris aquino <chris@radishmouse.com>
2019-04-25 13:21:05 -0400

    adds skeleton of GenreList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import movies from './movies';
+
+import GenreList from './GenreList';
+
+
 function App() {
+  // console.log(movies);
   return (
     <div className="App">
-
+      <GenreList genres={}/>
     </div>
   );
 }
---
+++ src/GenreList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GenreList(props) {
+    return (
+        <ul>
+            {/* list out those genres! */}
+
+        </ul>
+    );
+}
+
+export default GenreList;
