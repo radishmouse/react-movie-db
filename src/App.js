ba58d15d0e66ad6cff2c3b624c1c935f2a9cf00f
chris aquino <chris@radishmouse.com>
2019-04-25 13:47:15 -0400

    adds MovieList component with PropTypes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import movies from './movies';
 
 import GenreList from './GenreList';
+import MovieList from './MovieList';
 
 
 function App() {
-  // console.log(movies);
+  // console.log(Object.keys(movies));
   return (
     <div className="App">
-      <GenreList genres={}/>
+      <GenreList genres={Object.keys(movies)}/>
+      <MovieList movies={Object.keys(movies.documentaries)}/>
     </div>
   );
 }
---
+++ src/MovieList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function MovieList(props) {
+    return (
+        <ul>
+            {
+                props.movies.map((movie, i) => <li key={i}>{movie}</li>)
+            }
+        </ul>
+    );
+}
+
+MovieList.propTypes = {
+    movies: PropTypes.array.isRequired
+}
+
+export default MovieList;
